34afc9eaf6994ce3f789fc9f8d9efe490099d28d
Rockey C.R <rockeyrai234@gmail.com>
2025-03-13 09:17:19 +0545

    completing the About section

--- award/src/App.jsx
+++ award/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
 
 const App = () => {
   return (
 <main className='relatice min-h-screen w-screen overflow-x-hidden'>
   <Hero/>
+  <About/>
 </main>
   )
 }
